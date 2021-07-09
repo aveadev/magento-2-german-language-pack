d9fc4cc99fd40875a5f3afd45e6827e3704acbc4
Matus <kobe21@gmail.com>
2021-07-09 10:15:07 +0200

    first init

---
+++ README.md
@@ -0,0 +1,11 @@
+## Magento 2 German Language Pack
+
+**Install German language pack**:
+
+``` php
+composer require aveadev/magento-2-german-language-pack:dev-master
+php bin/magento setup:static-content:deploy de_DE
+php bin/magento indexer:reindex
+php bin/magento cache:clean
+php bin/magento cache:flush
+```
---
+++ registration.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *                     aveadev/magento-2-german-language-pack package
+ *                     NOTICE OF LICENSE
+ *
+ *                     This source file is subject to the MIT License
+ *                     that is bundled with this package in the file LICENSE.txt.
+ *                     It is also available through the world-wide-web at this URL:
+ *                     https://www.avea.dev/
+ *
+ *                     @category  Avea
+ *                     @package   aveadev/magento-2-german-language-pack
+ *                     @copyright Copyright (c) 2019
+ *                     @license   https://www.avea.dev/
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'aveadev_de_de',
+    __DIR__
+);
